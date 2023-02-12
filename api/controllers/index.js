8f3a4b8d7eba8c2935a9cef6b82d918700831380
Phil <philipdutoit@yahoo.com>
2023-02-12 08:31:50 +0100

    Extend controller logic by adding db queries

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -1,20 +1,44 @@
+const connection = require("../db/config")
+
 exports.getAllTodos = (req, res, next) => {
-    res.status(201).json({
-        status: "success",
-        message: "get!",
+    connection.query("SELECT * FROM todolist", (err, data, fields) => {
+        if (err) return next("No todo data found")
+        res.status(200).json({
+            status: "success",
+            data: data,
+        })
     })
 }
 
 exports.createTodo = (req, res, next) => {
-    res.status(201).json({
-        status: "success",
-        message: "created todo!",
-    })
+    if (!req.body) return next("No form data found")
+    const values = [req.body.name, "pending"]
+    connection.query(
+        "INSERT INTO todolist (name, status) VALUES(?)",
+        [values],
+        (err, data, fields) => {
+            if (err) return next(err)
+            res.status(201).json({
+                status: "success",
+                message: "todo created!",
+            })
+        }
+    )
 }
 
 exports.updateTodo = (req, res, next) => {
-    res.status(201).json({
-        status: "success",
-        message: "todo updated!",
-    })
+    if (!req.params.id) {
+        return next("No todo id found", 404)
+    }
+    connection.query(
+        "UPDATE todolist SET status='completed' WHERE id=?",
+        [req.params.id],
+        (err, data, fields) => {
+            if (err) return next(err)
+            res.status(201).json({
+                status: "success",
+                message: "todo updated!",
+            })
+        }
+    )
 }
